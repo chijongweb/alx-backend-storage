b7e4525fde512f5a0cd14fcbc030b6c8308ba7e7
chijongweb <160375788+chijongweb@users.noreply.github.com>
2025-07-12 13:51:56 +0200

    Update web.py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -7,13 +7,12 @@ import redis
 import requests
 from functools import wraps
 
-redis_client = redis.Redis()
-
 
 def count_url_access(method):
     @wraps(method)
     def wrapper(url: str) -> str:
-        redis_client.incr(f"count:{url}")
+        r = redis.Redis()
+        r.incr(f"count:{url}")
         return method(url)
     return wrapper
 
@@ -22,13 +21,14 @@ def cache_page(expiration: int = 10):
     def decorator(method):
         @wraps(method)
         def wrapper(url: str) -> str:
+            r = redis.Redis()
             cache_key = f"cache:{url}"
-            cached_data = redis_client.get(cache_key)
+            cached_data = r.get(cache_key)
             if cached_data:
-                return cached_data.decode('utf-8')
-            page_content = method(url)
-            redis_client.setex(cache_key, expiration, page_content)
-            return page_content
+                return cached_data.decode("utf-8")
+            content = method(url)
+            r.setex(cache_key, expiration, content)
+            return content
         return wrapper
     return decorator
 
